2b88258c60a1800886389496050e0387685d3563
Anh Pham <anhnnpham@gmail.com>
2019-12-14 18:46:25 +0200

    inifinite loop

--- debug.cpp
+++ debug.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include<sstream>
+#include <sstream>
 
-#include <time.h> //added for the random number generator seed
-#include <cstdlib>//added to use the rand function
+#include <time.h>  //added for the random number generator seed
+#include <cstdlib> //added to use the rand function
 
 int main()
 {
@@ -10,28 +10,39 @@ int main()
     std::string userString; // input = ALL int, char, string
     int guess = -1;
 
-    srand(time(NULL));  //set the seed for the random number generator
-    target = rand() %100 + 1; //generate the 'random' number
+    srand(time(NULL));         //set the seed for the random number generator
+    target = rand() % 100 + 1; //generate the 'random' number
 
     while (guess != target)
     {
-        std::cout<<"Guess a number between 0 and 100: ";
-        std::getline (std::cin,userString);
+        std::cout << "Guess a number between 0 and 100: ";
+        std::getline(std::cin, userString);
         //convert to an int
         std::stringstream(userString) >> guess;
-        std::cout<<userString<<"\n"; // in case it's a char or string
-        
-        if(guess > target)
-            std::cout<<"Your guess is too high\n";
-        else if(guess < target)
-            std::cout<<"Your guess is too low\n";
+        std::cout << userString << "\n"; // in case it's a char or string
+
+        if (guess > target)
+            std::cout << "Your guess is too high\n";
+        else if (guess < target)
+            std::cout << "Your guess is too low\n";
         else
-            std::cout<<"You guessed the target!\n";
-        if(userString == "q") // bc = string
+            std::cout << "You guessed the target!\n";
+        if (userString == "q") // bc = string
         {
-            std::cout<<"The number was "<<target<<"\n";
+            std::cout << "The number was " << target << "\n";
             break; // out of while()
         }
     }
+    
+    for (;;)
+    {
+        std::cout << "This for loop will run forever\n";
+    }
+
+    while (1)
+    {
+        std::cout << "This while loop will run forever\n";
+    }
+
     return 0;
 }
