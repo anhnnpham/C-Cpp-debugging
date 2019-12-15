b550312ce5557620f5beded5ec6d199a5290db9c
Anh Pham <anhnnpham@gmail.com>
2019-12-15 10:56:53 +0200

    sort array

--- debug.cpp
+++ debug.cpp
@@ -1,40 +1,57 @@
-
-//*Sample program for Pointers*/
+/* Goal: Practice array manipulation in C++. 
+The user will input 40 integers. 
+**Put them into an array. Then print the array in the order the numbers were
+**entered. Then print in reverse order. Then sort the array in ascending order 
+**and print it. 
+**separate the numbers in the array by one space. 
+**For example: the array were [3,4,55] the printout would be 3 4 55
+ */
 
 #include <iostream>
-#include <string>
-
+#include <stdio.h>
+using namespace std;
 int main()
 {
-    int givenInt;
-    float givenFloat;
-    double givenDouble;
-    std::string givenString;
-    char givenChar;
-    int *pointerGivenInt, **pointerPointerGivenInt;
+    int userInput[41], counter = 0;
+    for(int i = 0; i < 41; i++)
+    {
+        cin.ignore();
+        cin >> userInput[i];
+        ++counter;
+    }
 
-    pointerGivenInt = &givenInt;
-    pointerPointerGivenInt = &pointerGivenInt;
+    for (int i = 0; i < 41; i++)
+    {
+        cout << userInput[i] << " ";
+    }
+    cout << "\n";
 
-    std::cin >> givenDouble;    
-    //We need to use cin.ignore so cin will ignore
-    //the characters in the buffer leftover 
-    //from the givenDouble
-    std::cin.ignore();
-    std::cin >> givenChar;
-    // same
-    std::cin.ignore();
-    std::getline(std::cin, givenString);
+    while (counter > 0)
+    {
+        cout << userInput[counter - 1] << " ";
+        --counter;
+    }
+    cout << "\n";
 
-    std::cout << "string = " << givenString << "\n";
-    std::cout << "character = " << (char) givenChar << "\n\n"; // y typecast?
+    // sort array 
+    int swap = 0;
+    for (int i = 0; i < 41; i++) 
+    {
+        for (int j = 0; j < 40; j++) // damn 40 not 41!
+        {
+            if (userInput[j] > userInput[j + 1])
+            {
+                swap = userInput[j];
+                userInput[j] = userInput[j + 1];
+                userInput[j + 1] = swap;
+            }
+        }
+    }
 
-    std::cout << "address string = " << &givenString << "\n"; // not like C
-    std::cout << "address character = " << (void *) &givenChar << "\n\n"; 
-        // y typecast? y not (char *)?
-
-    std::cout << "value of givenInt = " << *pointerGivenInt << "\n";
-    std::cout << "double dereference of givenInt = " << **pointerPointerGivenInt << "\n"; 
-
+    for (int i = 0; i < 41; i++)
+    {
+        cout << userInput[i] << " ";
+    }
+    cout << "\n";
     return 0;
 }
