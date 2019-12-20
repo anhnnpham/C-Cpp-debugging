aca67694ce48195c91f0442ca815477301b68d6e
Anh Pham <anhnnpham@gmail.com>
2019-12-21 00:14:28 +0200

    class

--- currentDebug.cpp
+++ currentDebug.cpp
@@ -1,62 +1,83 @@
-/* Goal: sort students by multiple data vaule
-**
-**student1: Name: Joe Lime, ID#15, Grad. Date: 2019
-**student2: Name: Bob Green, ID#3, Grad. Date: 2020
-**student3: Name: SallyAnne Green , ID#1, Grad. Date: 2017
-**student4: Name: Annie Blue, ID#10, Grad. Date: 2020
-**student5: Name: Jose Lemon, ID#25, Grad. Date: 2016
-You will need to sort the students by name, then sort them by number,
-and finally sort them by graduation date.
-After each sort print out all the information on each of the students.
- 
-Concatenate data on each student into a string. Then sort based on those strings. 
-This would be a beast to do. You would have to keep track of which parts of the string 
-need to be converted back to integers, otherwise you will be sorting numbers 
-alphabetically. Also, the names are of different lengths, so you would have to add some 
-kind of string delimiter to separate the fields.
-
-Maybe create some kind of array of pointers. This would be a total misuse of pointers and 
-would not be recommended as good programming.... but it might work.
-
-You could learn to use tuples. We haven't gotten to that yet, but you might be able to 
-write something that might not be too, too horrible.
-*/
-// #include "main.hpp"
-// in main.hpp
-#include <string>
 #include <iostream>
 using namespace std;
+class Student
+{
+    string name;
+    int id;
+    int gradDate;
+
+public:
+    void setName(string nameIn);
+    void setId(int idIn);
+    void setGradDate(int dateIn);
+    string getName();
+    int getId();
+    int getGradDate();
+    void print();
+};
+
+void Student::setName(string nameIn)
+{
+    name = nameIn;
+}
+
+void Student::setId(int idIn)
+{
+    id = idIn;
+}
+
+void Student::setGradDate(int gradDateIn)
+{
+    gradDate = gradDateIn;
+}
+
+void Student::print()
+{
+    cout << name << " " << id << " " << gradDate;
+}
+
+string Student::getName()
+{
+    return name;
+}
+
+int Student::getId()
+{
+    return id;
+}
+
+int Student::getGradDate()
+{
+    return gradDate;
+}
 
 int main()
 {
-    //ToDo sort the students by name, then by number, then by gradDate
-    const int size = 3;
-    const int chtr = 20;
-    string student1[size][chtr] = {"Joe Lime", "15", "2019"};
-    string student2[size][chtr] = {"Bob Green", "3", "2020"};
-    string student3[size][chtr] = {"SallyAnne Green", "1", "2017"};
-    string student4[size][chtr] = {"Annie Blue", "10", "2020"};
-    string student5[size][chtr] = {"Jose Lemon", "25", "2016"};
-    string *collection[5] = {&student1[size][chtr], &student2[size][chtr], &student3[size][chtr], &student4[size][chtr], &student5[size][chtr]};
-    // sort by name
-    for (size_t index = 0; index < 4; index++)
-    {
-        int delimit = 0;
-        while ((*collection[index])[0][delimit] != '\0')
-        {
-            if ((*collection[index])[0][delimit] > (*collection[index + 1])[0][delimit])
-            {
-                string *swap = collection[index + 1];
-                collection[index + 1] = collection[index];
-                collection[index] = swap;
-            }
-            ++delimit;
-        }
-    }
+    int integer1;
+    float float1;
+    Student student1; // obj
 
-    for (size_t index = 0; index < 5; index++)
-    {
-        cout << (*collection[index])[0] << "\n";
-    }
+    integer1 = 4;   //assign a value to integer1
+    float1 = 4.333; //assign a value to float1
+    // string as parameter
+    student1.setName("Catherine Gamboa"); //assign a value to the student name
+    student1.setId(54345);                //assign a value to the student id number
+    student1.setGradDate(2017);           //assign a value to the student grad date
+
+    cout << "integer1 = " << integer1 << "\n";
+    cout << "float1 = " << float1 << "\n\n";
+
+    //There are two ways we can print the values of our class:
+    //The first is to call the print function we created.
+    cout << "Using Student::print()\n";
+    cout << "Student1 = ";
+    student1.print();
+    cout << "\n\n";
+
+    //The second is to access each member of the class using the get functions
+    cout << "Using the student access functions\n";
+    cout << "Student1 name = " << student1.getName() << "\n";
+    cout << "Student1 ID = " << student1.getId() << "\n";
+    cout << "Student1 Grad Date = " << student1.getGradDate() << "\n";
     return 0;
 }
