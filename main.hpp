a6849b1be74d3b978c516541c66d04be9abad40c
Anh Pham <anhnnpham@gmail.com>
2019-12-24 15:21:56 +0200

    overloaded '+' w (totLen * totWidth)

--- main.hpp
+++ main.hpp
@@ -1,157 +1,44 @@
-#include<iostream>
+#include <iostream>
+using namespace std;
+/* TODO: Add all necessary functions and 
+Overload the '+' sign to:
+Add the lengths of the two shapes together
+Add the widths of the two shapes together
+Calculate and return the area of the new shape
 
-//C++ allows us to 'overload' the same function name as long as
-//the variable types in the function inputs are different.
-/* Add two more variations on findSmaller;
-create functions that accept three arguments for each of the variable types
-create functions that accept an array for each variable type. */
-class Compare
+For example: If there are two shapes s1 = 3x4 and s2 = 2x3
+s1 + s2 = s3 where s3 has dimensions (3+2 = 5) *(4 + 3 = 7) = 5 x 7 = 35
+ */
+class Shape
 {
+private:
+    int length; // Length of a box
+    int width;
+
 public:
-    int findSmaller(int input1, int input2);
-    float findSmaller(float input1, float input2);
-    char findSmaller(char input1, char input2);
+    // Constructor definition
+    Shape(int l = 2, int w = 2)
+    {
+        length = l;
+        width = w;
+    }
+    double Area()
+    {
+        return length * width;
+    }
+/*     int getLength()
+    {
+        return length;
+    }
+    int getWidth()
+    {
+        return width;
+    } */
 
-    int findSmaller(int input1, int input2, int input3);
-    float findSmaller(float input1, float input2, float input3);
-    char findSmaller(char input1, char input2, char input3);
-
-    int findSmaller(int arrInput[], int size);
-    float findSmaller(float arrInput[], float size);
-    char findSmaller(char arrInput[], char size);
+    int operator + (Shape sh2)
+    {
+        return (length + sh2.length) * (width + sh2.width);
+        // Method 2:
+        // return (length + sh2.getLength()) * (width + sh2.getWidth());
+    }
 };
-
-int Compare::findSmaller(int input1, int input2)
-{
-    if(input1<input2)
-        return input1;
-    return input2;
-}
-float Compare::findSmaller(float input1, float input2)
-{
-    if(input1<input2)
-        return input1;
-    return input2;
-}
-
-char Compare::findSmaller(char input1, char input2)
-{
-    if(input1<input2)
-        return input1;
-    return input2;
-}
-
-int Compare::findSmaller(int input1, int input2, int input3)
-{
-    if (input1 < input2)
-    {
-        if (input1 < input3)
-        {
-            return input1;
-        }
-    }
-    else if (input2 < input3)
-    {
-        if (input2 < input1)
-        {
-            return input2;
-        }
-    }
-    return input3;
-}
-float Compare::findSmaller(float input1, float input2, float input3)
-{
-    if (input1 < input2)
-    {
-        if (input1 < input3)
-        {
-            return input1;
-        }
-    }
-    else if (input2 < input3)
-    {
-        if (input2 < input1)
-        {
-            return input2;
-        }
-    }
-    return input3;
-}
-char Compare::findSmaller(char input1, char input2, char input3)
-{
-    if (input1 < input2)
-    {
-        if (input1 < input3)
-        {
-            return input1;
-        }
-        else
-        {
-            return input3;
-        }
-    }
-    else
-    {
-        if (input2 < input3)
-        {
-            return input2;
-        }
-        else
-        {
-            return input3;
-        }
-    }
-
-    // Method 2:
-    if (input1 < input2)
-    {
-        if (input1 < input3)
-        {
-            return input1;
-        }
-    }
-    else if (input2 < input3)
-    {
-        return input2;
-    }
-    else 
-    {
-        return input3;
-    }
-}
-int Compare::findSmaller(int arrInput[], int size)
-{
-    int min = arrInput[0];
-    for (size_t i = 1; i < size; i++)
-    {
-        if (arrInput[i] < min)
-        {
-            min = arrInput[i];
-        }
-    }
-    return min;
-}
-float Compare::findSmaller(float arrInput[], float size)
-{
-    float min = arrInput[0];
-    for (size_t i = 1; i < size; i++)
-    {               
-        if (arrInput[i] < min)
-        {
-            min = arrInput[i];
-        }
-    }
-    return min;
-}
-char Compare::findSmaller(char arrInput[], char size)
-{
-    char min = arrInput[0];
-    for (size_t i = 1; i < size; i++)
-    {               
-        if (arrInput[i] < min)
-        {
-            min = arrInput[i];
-        }
-    }
-    return min;
-}
