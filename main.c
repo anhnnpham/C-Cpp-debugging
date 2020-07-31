ffabf12ac4c5594102cd9266823e75873eeed5e9
anhpham <anh.pham@nokia.com>
2020-08-01 00:34:09 +0300

    function pointer, arr of functions

--- main.c
+++ main.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
-/* Create a union that stores an array of 21 characters including '\0' and 6 ints */
-typedef union {
-  // order matters for (initializer lists)
-  int intArr[6]; // byte[0] -> 23
-  int intArr0, intArr1, intArr2, intArr3, intArr4, intArr5; // TODO: DOESNT WORK!!!
-  char chars[21];
-} abcUnion;
+/* Complete the array of pointers to functions and call each function using 
+ * its pointer from the array. Array of pointers to functions? */
+void f1(int var)
+{
+  printf("this is f1 and var is: %d\n", var);
+}
 
-int main() {
-    // TODO: initializer lists are assigned to the First member of the union/struct -> union var order matters!
-    // TODO: 6 ints here outputs "I understand Unions!"
-    abcUnion intCharacters = {1853169737, 1936900, 1684955508, 1768838432, 561213039, 0}; // TODO: byte[20] -> 23 = '\0' since (0 = NULL in ASCII)
+void f2(int var)
+{
+  printf("this is f2 and var is: %d\n", var);
+}
+
+void f3(int var)
+{
+  printf("this is f3 and var is: %d\n", var);
+}
+
+int main()
+{
+  /* define an array full of function pointers to the above functions, that take an `int` as their only argument */
+  void (*f3_ptr)(int);
+  f3_ptr = &f3;
   
-    for(int i = 0; i < 19; ++i) // byte[16] -> 19
-        printf("%c, ", intCharacters.chars[i]);
-    printf("%c\n", intCharacters.chars[19]); // '!', [20] = '\0'
-    printf("%s\n", intCharacters.chars);
+  void(*funcPtr[])(int)  = {f1, f2, f3};
+  
+  int c = 0;
+  while (c < 3)
+  {
+    /* call the functions using the function pointers of the array at index `c` with `c` as an argument */
+    funcPtr[c](c);
+    ++c;
+  }
+  return 0;
 }
